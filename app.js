752319ed0b8c8cc70148a19567ee67d0bf413fa9
ramyaravindranath <ramya.ravindranathp@gmail.com>
2016-08-04 22:00:11 +1000

    fixes indentation

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 angular.module('movieApp', [])
-  .controller('movieController', function($scope, $http){
-     if($scope.movie === undefined){
-	$scope.movie="Harry Potter";
-	fetchMovie();
+  .controller('movieController', function($scope, $http) {
+    if ($scope.movie === undefined) {
+      $scope.movie = "Harry Potter";
+      fetchMovie();
     }
 
     // For changing search on search tab
-	 
-	 $scope.newSearch = function(){
-       setTimeout(fetchMovie, 500);
+
+    $scope.newSearch = function() {
+      setTimeout(fetchMovie, 500);
     };
 
     // calling api to fetch movie details
-	 function fetchMovie(){
+    function fetchMovie() {
       $http.get("http://www.omdbapi.com/?t=" + $scope.movie + "&plot=full")
-       .success(function(response){ $scope.MovieDetails = response; });
+        .success(function(response) {
+          $scope.MovieDetails = response;
+        });
 
       $http.get("http://www.omdbapi.com/?s=" + $scope.movie)
-       .success(function(response){  $scope.OtherDetails = response; });
-    }});
+        .success(function(response) {
+          $scope.OtherDetails = response;
+        });
+    }
+});
